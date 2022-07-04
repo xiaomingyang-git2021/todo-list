56a274deb67d67cf18eb1400e94329b46dda5455
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-07-04 18:57:32 -0400

    add props.id into htmlFor property

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ export default function Todo(props) {
     <li className="todo stack-small">
       <div className="c-cb">
         <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-        <label className="todo-label" htmlFor="todo-0">
+        <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
       </div>
