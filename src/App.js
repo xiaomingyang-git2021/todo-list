dd6a14a7a6a1f44caffe5dbfadec6ecd3da4c93e
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-07-04 20:30:41 -0400

    create two components: Form and FilterButton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 import './index.css'
 
 
@@ -15,7 +17,8 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
+      <Form />
+      {/* <form>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
@@ -31,9 +34,12 @@ function App(props) {
         <button type="submit" className="btn btn__primary btn__lg">
           Add
         </button>
-      </form>
+      </form> */}
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+        {/* <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show</span>
           <span>all</span>
           <span className="visually-hidden">tasks</span>
@@ -47,7 +53,7 @@ function App(props) {
           <span className="visually-hidden">Show</span>
           <span>Completed</span>
           <span className="visually-hidden">tasks</span>
-        </button>
+        </button> */}
       </div>
       <h2 id="list-heading">
         3 tasks remaining
