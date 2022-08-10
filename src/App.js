a04e019210d8b5eebc4f14c58d92aef60e046a88
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-08-09 20:57:59 -0400

    Editing the name of a task, A UI for editing, Conditional rendering, Toggling the <Todo /> templates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import './index.css'
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new obkect
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+        if (id === task.id) {
+          //
+          return {...task, name: newName}
+        }
+        return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
   
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
   
-  const taskList = tasks.map(task => (
-      <Todo
-        id={task.id} 
-        name={task.name} 
-        completed={task.completed}
-        key={task.id} 
-      />
-    )
-  );
+  // const taskList = tasks.map(task => (
+  //     <Todo
+  //       id={task.id} 
+  //       name={task.name} 
+  //       completed={task.completed}
+  //       key={task.id} 
+  //     />
+  //   )
+  // );
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
